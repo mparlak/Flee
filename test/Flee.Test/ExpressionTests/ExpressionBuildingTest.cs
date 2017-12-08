bb004332e24a2eb668bbe781b8332528e800cba4
Espen Skaufel <espen@skaufel.net>
2017-12-08 21:53:49 +0100

    Fix for issue 13, Expressions as variables causes InvalidCastException

---
+++ test/Flee.Test/ExpressionTests/ExpressionBuildingTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Flee.PublicTypes;
+
+namespace ExpressionBuildingTest
+{
+    [TestClass]
+    public class ExpressionBuildingTest
+    {
+        [TestMethod]
+        public void ExpressionsAsVariables()
+        {
+            ExpressionContext context = new ExpressionContext();
+            context.Imports.AddType(typeof(Math));
+            context.Variables.Add("a", 3.14);
+            IDynamicExpression e1 = context.CompileDynamic("cos(a) ^ 2");
+
+            context = new ExpressionContext();
+            context.Imports.AddType(typeof(Math));
+            context.Variables.Add("a", 3.14);
+
+            IDynamicExpression e2 = context.CompileDynamic("sin(a) ^ 2");
+
+            // Use the two expressions as variables in another expression
+            context = new ExpressionContext();
+            context.Variables.Add("a", e1);
+            context.Variables.Add("b", e2);
+            IDynamicExpression e = context.CompileDynamic("a + b");
+
+            Console.WriteLine(e.Evaluate());
+        }
+    }
+}
