a3c35c8f91ae0c7b4dd4aa6a00bc2f8e5beeeac1
bondjames12 <colinweber@gmail.com>
2019-03-28 13:19:20 -0600

    Add Extension method unit tests

---
+++ test/Flee.Test/ExtensionMethodTests/TestData.cs
@@ -0,0 +1,25 @@
+﻿namespace Flee.ExtensionMethodTests.ExtensionMethodTestData
+{
+    using Microsoft.Win32.SafeHandles;
+
+    internal class TestData
+    {
+        public string Id { get; set; }
+
+        public TestData Sub
+        {
+            get { return new TestData { Id = "Sub" + this.Id }; }
+        }
+
+        public string SayHello(int times)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < times; i++)
+            {
+                result += "hello ";
+            }
+
+            return result + Id;
+        }
+    }
+}
---
+++ test/Flee.Test/ExtensionMethodTests/TestDataExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Flee.ExtensionMethodTests.ExtensionMethodTestData
+{
+    internal static class TestDataExtensions
+    {
+        public static string SayHello(this TestData data)
+        {
+            return "Hello " + data.Id;
+        }
+
+        public static string SayHello(this TestData data, string suffix)
+        {
+            return "Hello " + data.Id + suffix;
+        }
+
+        public static string SayHello(this TestData data, bool friendly)
+        {
+            return "Hello " + (friendly ? "dear " : string.Empty) + data.Id;
+        }
+    }
+}
